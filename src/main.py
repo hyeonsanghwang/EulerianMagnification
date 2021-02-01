8bf2795a4d037112672e55dfb3335a74f7d402df
hyeonsanghwang <hyeonsang92@gmail.com>
2021-02-01 18:08:49 +0900

    Add utils module & Create main function

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+from utils.camera.video_stream import VideoStream
+
+
+CAMERA_NUM = 0
+
+FRAME_WIDTH = 320
+FRAME_HEIGHT = 240
+FPS = 30
+
+
+def get_frames(stream, event_key=13):
+    processing = False
+    frames = []
+    while True:
+        ret, frame = stream.read()
+        if processing:
+            frames.append(frame)
+
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(stream.delay())
+        if key == 27:
+            break
+        elif key == event_key:
+            if processing:
+                break
+            else:
+                processing = True
+    cv2.destroyAllWindows()
+    stream.release()
+    return frames
+
+
+if __name__ == "__main__":
+    stream = VideoStream(CAMERA_NUM, FPS, FRAME_WIDTH, FRAME_HEIGHT)
+    frames = get_frames(stream, event_key=13)
+    print(np.shape(frames))
+
